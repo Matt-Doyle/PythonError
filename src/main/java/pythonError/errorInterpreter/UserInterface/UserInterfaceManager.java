f80b2321d6bbdf440b26b9dde7239107c82db147
Matt-Doyle <doy0001@jmss.vic.edu.au>
2016-05-28 17:13:55 +1000

    Actually added a basic text entry and scrollbar

--- src/main/java/pythonError/errorInterpreter/UserInterface/UserInterfaceManager.java
+++ src/main/java/pythonError/errorInterpreter/UserInterface/UserInterfaceManager.java
@@ -8,12 +8,21 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 
+import pythonError.errorInterpreter.UserInterface.TracebackEntry;
+
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.InlineCssTextArea;
+
 public class UserInterfaceManager {
     private StackPane root;
+    private TracebackEntry traceback;
     public boolean initialise(Stage primaryStage) {
         primaryStage.setTitle("Python Error Interpreter - By Matthew Doyle and Christopher Hall");
 
-        root = new StackPane();
+        traceback = new TracebackEntry();
+        InlineCssTextArea RichTextEntry = traceback.initialise();
+
+        root = new StackPane(new VirtualizedScrollPane(RichTextEntry) );
 
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
