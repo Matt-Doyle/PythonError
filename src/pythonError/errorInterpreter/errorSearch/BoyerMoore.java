5c52ce64783a9df891e6e2a791d9d4476df6339a
Matt-Doyle <doy0001@jmss.vic.edu.au>
2016-05-30 08:55:10 +1000

    Added Boyer-Moore Search Algorithm

--- src/pythonError/errorInterpreter/errorSearch/BoyerMoore.java
+++ src/pythonError/errorInterpreter/errorSearch/BoyerMoore.java
@@ -1,7 +1,97 @@
 package pythonError.errorInterpreter.errorSearch;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
 /**
  * Created by Matthew Doyle on 29/05/2016.
  */
-public class BoyerMoore {
+public class BoyerMoore { // Implementation derived from http://www.cs.utexas.edu/~moore/publications/fstrpos.pdf
+    public HashMap<Character, Number> GenerateBCRLookupTable(String Needle) {
+        HashMap<Character, Number> BCRMap = new HashMap<Character, Number>();
+
+        for (int i = 0; i < Needle.length(); i++) {
+            char lookupCharacter = Needle.charAt(i);
+            BCRMap.put(lookupCharacter, Math.max(1, Needle.length() - 1 - i));
+        }
+        return BCRMap;
+    }
+
+    public int[] GenerateGSRLookupTable(String Needle) {
+        int m = Needle.length();
+
+        int[] suffixes = new int[m];
+
+        suffixes[m - 1] = m;
+        int f = 0;
+
+        int g = m - 1;
+        for(int i = m - 2; i >= 0; i--) {
+            if (i > g && suffixes[i + m - 1 - f] < i - g) {
+                suffixes[i] = suffixes[i + m - 1 - f];
+            } else {
+                if (i < g) {
+                    g = i;
+                }
+                f = i;
+                while(g >= 0 && Needle.substring(g, g) == Needle.substring(g + m - 1 - f, g + m - 1 - f)) {
+                    g--;
+                }
+                suffixes[i] = f - g;
+            }
+        }
+
+        int[] goodSuffixes = new int[Needle.length()];
+
+        for (int goodSuffix : goodSuffixes) {
+         goodSuffixes[goodSuffix] = m;
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            if (suffixes[i] == i + 1) {
+                for (int j = 0; j < m - 1 - i; j++) {
+                    if (goodSuffixes[j] == m) {
+                        goodSuffixes[j] = m - 1 - i;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < m - 2; i++) {
+            goodSuffixes[m - 1 - suffixes[i]] = m - 1 - i;
+        }
+        return goodSuffixes;
+    }
+
+    public ArrayList<Number> search(String needle, String haystack) {
+        int[] GSR = GenerateGSRLookupTable(needle);
+        HashMap<Character, Number> BCR = GenerateBCRLookupTable(needle);
+        return precomputedSearch(needle, haystack, GSR, BCR);
+    }
+
+    public ArrayList<Number> precomputedSearch(String needle, String haystack, int[] GSR, HashMap<Character, Number> BCR) {
+        ArrayList<Number> result = new ArrayList<Number>();
+        int m = needle.length();
+        int offset = 0;
+        int searchArea = haystack.length() - needle.length() - 1;
+        while (offset < searchArea) {
+            int i = m - 1;
+            while (i >= 0 && needle.charAt(i) == haystack.charAt(offset + i))
+                i--;
+            if (i < 0) {
+                result.add(offset);
+                offset += needle.length();
+            } else {
+                offset += Math.max(GSR[i], BCR.containsKey(haystack.charAt(i)) ? BCR.get(haystack.charAt(i)).intValue() : m);
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        BoyerMoore bm = new BoyerMoore();
+        for (Number i : bm.search("test", "testing testing testing")) {
+            System.out.println(i);
+        }
+    }
 }
