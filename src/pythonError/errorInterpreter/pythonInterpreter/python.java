551c161b8d6851726591e6df550f296b4de0d3dd
Matt-Doyle <doy0001@jmss.vic.edu.au>
2016-05-30 17:23:14 +1000

    Created Python.java, contains methods relevant to stack traceback interpretation.

--- src/pythonError/errorInterpreter/pythonInterpreter/python.java
+++ src/pythonError/errorInterpreter/pythonInterpreter/python.java
@@ -4,12 +4,33 @@ package pythonError.errorInterpreter.pythonInterpreter;
  * Created by Matthew Doyle on 30/05/2016.
  */
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.util.regex.MatchResult;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import pythonError.errorInterpreter.errorSearch.BMPattern;
+import pythonError.errorInterpreter.errorSearch.BoyerMoore;
 
 public class python {
-    Pattern parentheses = Pattern.compile("\\(|\\)");
-    Pattern braces = Pattern.compile("\\{|\\}");
-    Pattern brackets = Pattern.compile("\\[|\\]");
+
+    private ArrayList<BMPattern> Exceptions;
+    private HashMap<String, String> Explanations;
+
+    python() {
+        Exceptions = new ArrayList<>();
+        Explanations = new HashMap<>();
+    }
+
+    public void AddException(String Exception, String Explanation) { // Creates objects
+        Exceptions.add(BoyerMoore.compile(Exception));
+        Explanations.put(Exception, Explanation);
+    }
+
+    public ArrayList<BMPattern> getExceptionList() { // Returns list of exceptions
+        return Exceptions;
+    }
+
+    public String getExplanation(String Exception) { // Returns the explanation for an exception
+        assert Explanations.containsKey(Exception);
+        return Explanations.get(Exception);
+    }
 }
