8eacb35591adccaf1cec38370544d63b5662d9a1
Matt-Doyle <doy0001@jmss.vic.edu.au>
2016-05-31 15:56:46 +1000

    Started working on better input parser.

---
+++ src/pythonError/errorInterpreter/inputPhaser/Token.java
@@ -0,0 +1,21 @@
+package pythonError.errorInterpreter.inputPhaser;
+
+/**
+ * Created by Matthew Doyle on 31/05/2016.
+ */
+
+
+enum tokens {
+    newLine,
+    keyWord,
+    stringLiteral,
+    number,
+    comment,
+}
+
+public class Token {
+    tokens type;
+    public Token(tokens type) {
+
+    }
+}
