1fdd494a3f06be2bee961a15e574060496987fa0
Matt-Doyle <doy0001@jmss.vic.edu.au>
2016-05-31 17:04:01 +1000

    Progressed with better code parser.

--- src/pythonError/errorInterpreter/inputPhaser/InputParser.java
+++ src/pythonError/errorInterpreter/inputPhaser/InputParser.java
@@ -5,6 +5,7 @@ import pythonError.errorInterpreter.pythonInterpreter.Python;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * Created by Christopher on 30/05/2016.
@@ -30,4 +31,57 @@ public class InputParser {
 		int lineNumber = Integer.parseInt(tracebackWords.get(tracebackWords.lastIndexOf("line") + 1).replace(",", ""));
 		return new LinedError(error, lineNumber);
 	}
+}
+
+
+class CodeParser {
+    static String[] KEYWORDS = {"False", "class", "finally", "is", "return", "None", "continue", "for", "lambda", "try", "True", "def", "from", "nonlocal",
+    "while", "and", "del", "global", "not", "with", "as", "elif", "if", "or", "yield", "assert", "else", "import", "pass", "break", "except", "is", "raise"};
+    static String[] OPERATORS = {"+", "-", "*", "/", "//", "%", "**", "==", "!=", "<", ">", "<=", ">=", "is", "in", "and", "or", "not", "&", "|", "~", "^", "<<",
+            ">>"};
+    static String[] DELIMITERS = {};
+
+    private int isKeyword(String input, int i) {
+        String currentChar = input.substring(i, i);
+        if (currentChar.matches("[A-Za-z_]")) {
+            for (String keyWord : KEYWORDS) {
+                if (i + (keyWord.length() - 1) > input.length()) {
+                    continue;
+                }
+                for (int j = 0; j < keyWord.length(); j++) {
+                    if (input.charAt(i + j) != keyWord.charAt(j)) {
+                        continue;
+                    }
+                }
+                if (i + keyWord.length() > input.length()) {
+                    return i + keyWord.length() - 1;
+                } else if (input.charAt(i + keyWord.length()) == ' ' || input.charAt(i + keyWord.length()) == '\n') {
+                    return i + keyWord.length() - 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public ArrayList<Token> constructTokens(String input) {
+        input.replace("\\\n", " ");
+
+
+        ArrayList<Token> tokenList = new ArrayList<>();
+        int previousIndent = 0;
+        int currentIndent = 0;
+
+        boolean isNewLine = true;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (isNewLine) {
+                int keywordOffset = isKeyword(input, i);
+                if (keywordOffset > 0) {
+
+                }
+            }
+        }
+
+        return new ArrayList<>();
+    }
 }
